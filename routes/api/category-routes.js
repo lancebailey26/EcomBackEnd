7ae6ea9861c99cf6fa158d6aca86d091fc87eadd
Lance Bailey <lancebailey222@gmail.com>
2021-07-27 20:02:04 -0400

    added routes + cleaned delete responses

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -80,7 +80,7 @@ router.delete('/:id', async (req, res) => {
     return;
   }
 
-  res.status(200).json(categoryData);
+  res.status(200).json({message: 'category deleted.'});
 } catch (err) {
   res.status(500).json(err);
 }
